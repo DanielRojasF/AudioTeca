7c922ed693de6fce3dbb7fa7e8f94dec9121e457
DanielRojasF <danielrf98@gmail.com>
2023-03-15 13:30:03 -0600

    Commit#1 Interfaz Grafica

---
+++ src/test/java/com/AudioTeca/AudioTecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AudioTeca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioTecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
