a565f5fbdc9b3778417dcf1e2ef6b21c541f6341
DanielRojasF <danielrf98@gmail.com>
2023-04-24 20:25:45 -0600

    Cambios en Index

---
+++ src/main/java/com/AudioTeca/controller/IndexController.java
@@ -0,0 +1,37 @@
+package com.AudioTeca.controller;
+
+import com.AudioTeca.Service.LibroService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
+@Controller
+@Slf4j
+public class IndexController {
+    
+    @Autowired
+    private LibroService libroService;
+    /*Es importante anotar que en 
+    Spring se utiliza la interface 
+    ClienteService, Spring busca cual 
+    clase implementa la interface y 
+    la utiliza de manera automática, 
+    así se separa aún más la 
+    implementación de la interface 
+    de la capa de presentación 
+    (Controller)
+    */
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+        var libros = libroService.getLibros();
+        model.addAttribute("libros", libros);
+        return "index";
+    }
+
+}
